74b631b4fad8c65ede9f9c673b3ce202b062ff5d
lehuyhiep831 <65733861+lehuyhiep831@users.noreply.github.com>
2020-11-16 23:33:55 +0700

    Add web api
    Co-Authored-By: Trang Ngô <57227966+NgoThiTrang-k17@users.noreply.github.com>

---
+++ WebAPI/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Data;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly DataContext dc;
+
+        public UserController(DataContext dc)
+        {
+            this.dc = dc;
+        }
+
+        [HttpGet] //GET api/user
+        public IActionResult GetUssers()
+        {
+            var users = dc.Users.ToList();
+            return Ok(users);
+        }
+        [HttpGet("{id}")] //GET api/user/id
+        public IActionResult Get(int id)
+        {        
+            var user = dc.Users.Where(r => r.Id == id); 
+            return Ok(user);
+        }
+    }
+}
