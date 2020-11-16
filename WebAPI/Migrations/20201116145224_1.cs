6274e7e5e2de6972e34c0c021ed952c4e0d682aa
lehuyhiep831 <65733861+lehuyhiep831@users.noreply.github.com>
2020-11-16 23:14:56 +0700

    Add WebAPI Project
    Co-Authored-By: Trang Ngô <57227966+NgoThiTrang-k17@users.noreply.github.com>

---
+++ WebAPI/Migrations/20201116145224_1.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebAPI.Migrations
+{
+    public partial class _1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(nullable: true),
+                    Lastname = table.Column<string>(nullable: true),
+                    Avatar = table.Column<int>(nullable: false),
+                    Email = table.Column<string>(nullable: true),
+                    Region = table.Column<string>(nullable: true),
+                    Gender = table.Column<int>(nullable: false),
+                    Self_introduce = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
