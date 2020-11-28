d5cdefe7420dc88da0fbadf32f09ed4b7d3565d0
lehuyhiep831 <lehuyhiep831@gmail.com>
2020-11-28 13:33:19 +0700

    add migration
    Co-Authored-By: Trang Ngô <57227966+NgoThiTrang-k17@users.noreply.github.com>

--- WebAPI/Migrations/20201128062139_3.Designer.cs
+++ WebAPI/Migrations/20201128062139_3.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebAPI.Data;
 
 namespace WebAPI.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20201128062139_3")]
+    partial class _3
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
