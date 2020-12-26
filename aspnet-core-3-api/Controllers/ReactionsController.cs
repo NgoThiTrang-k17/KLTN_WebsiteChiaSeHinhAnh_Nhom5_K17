f374641a568827c63be4d67b51a50b9d6b58707d
lehuyhiep831 <65733861+lehuyhiep831@users.noreply.github.com>
2020-12-26 14:46:47 +0700

    Update ReactionsController.cs

--- aspnet-core-3-api/Controllers/ReactionsController.cs
+++ aspnet-core-3-api/Controllers/ReactionsController.cs
@@ -5,7 +5,7 @@ using WebApi.Services;
 
 namespace WebApi.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class ReactionsController : BaseController
     {
