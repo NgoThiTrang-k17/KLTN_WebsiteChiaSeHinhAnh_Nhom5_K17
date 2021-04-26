3b4de34aaddb52701f43f3f6107397f99bdcbcfb
lehuyhiep831 <65733861+lehuyhiep831@users.noreply.github.com>
2021-04-26 21:37:59 +0700

    Add publish profile

--- sourcecode/aspnet-core-3-api/Models/Comments/CommentResponse.cs
+++ sourcecode/aspnet-core-3-api/Models/Comments/CommentResponse.cs
@@ -11,9 +11,9 @@ namespace WebApi.Models.Comments
         public string Content { get; set; }
         public DateTime Created { get; set; }
 
-        public int? ParentId { get; set; }
-        public bool IsParent => !ParentId.HasValue;
-        public bool IsChild => ParentId.HasValue;
+        public int? ParrentId { get; set; }
+        public bool IsParent => !ParrentId.HasValue;
+        public bool IsChild => ParrentId.HasValue;
 
         public int OwnerId { get; set; }
         public string OwnerName { get; set; }
