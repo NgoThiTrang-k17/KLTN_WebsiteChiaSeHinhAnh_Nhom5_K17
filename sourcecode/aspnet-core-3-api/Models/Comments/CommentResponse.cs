b51072b7b204b16848732eb4bf4ad2cd0329d4ca
lehuyhiep831 <65733861+lehuyhiep831@users.noreply.github.com>
2021-05-08 11:34:22 +0700

    Update CommentResponse.cs

--- sourcecode/aspnet-core-3-api/Models/Comments/CommentResponse.cs
+++ sourcecode/aspnet-core-3-api/Models/Comments/CommentResponse.cs
@@ -11,9 +11,9 @@ namespace WebApi.Models.Comments
         public string Content { get; set; }
         public DateTime Created { get; set; }
 
-        public int? ParrentId { get; set; }
-        public bool IsParent => !ParrentId.HasValue;
-        public bool IsChild => ParrentId.HasValue;
+        public int ParrentId { get; set; }
+        public bool IsParent => ParrentId == 0;
+        public bool IsChild => ParrentId != 0;
 
         public int OwnerId { get; set; }
         public bool IsCreatedByThisUser { get;  set; }
