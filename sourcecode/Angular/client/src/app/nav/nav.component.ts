7989cd8917bfa162b9d2bf71d9cff27e23fc5d62
lehuyhiep831 <lehuyhiep831@gmail.com>
2021-05-21 19:54:33 +0700

    Update client

--- sourcecode/Angular/client/src/app/nav/nav.component.ts
+++ sourcecode/Angular/client/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -9,7 +11,9 @@ import { AccountService } from '../_services/account.service';
 export class NavComponent implements OnInit {
   model: any= {}
   
-  constructor(public accountService: AccountService) { }
+  constructor(public accountService: AccountService,
+    private router: Router,
+    private toastr: ToastrService) { }
 
   ngOnInit(): void {
     
@@ -18,14 +22,16 @@ export class NavComponent implements OnInit {
   login(){
     this.accountService.login(this.model).subscribe(response => {
       console.log(response);
-
+      this.router.navigateByUrl('/members');
       },error =>{
       console.log(error);
+      this.toastr.error(error.error);
     })
   }
 
   logout(){
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 
 }
