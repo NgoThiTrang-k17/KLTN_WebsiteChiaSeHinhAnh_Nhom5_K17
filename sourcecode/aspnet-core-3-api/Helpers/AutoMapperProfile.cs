f442f9b0e8509f73fd97d2c2e3b645e7497ad89a
lehuyhiep831 <lehuyhiep831@gmail.com>
2021-07-05 00:40:19 +0700

    Update AutoMapperProfile.cs

--- sourcecode/aspnet-core-3-api/Helpers/AutoMapperProfile.cs
+++ sourcecode/aspnet-core-3-api/Helpers/AutoMapperProfile.cs
@@ -58,7 +58,17 @@ namespace WebApi.Helpers
 
             CreateMap<CreatePostRequest, Post>();
 
-            CreateMap<UpdatePostRequest, Post>();
+            CreateMap<UpdatePostRequest, Post>()
+                .ForAllMembers(x => x.Condition(
+                   (src, dest, prop) =>
+                   {
+                       // ignore null & empty string properties
+                       if (prop == null) return false;
+                       if (prop.GetType() == typeof(string) && string.IsNullOrEmpty((string)prop)) return false;
+
+                       return true;
+                   }
+               ));
             //Comment
             CreateMap<Comment, CommentResponse>();
 
